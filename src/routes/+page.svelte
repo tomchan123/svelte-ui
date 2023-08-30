2f28794f7f741f4abc3330680a1c18c850d230e5
Tom Chan <tomchan@qqueenyapp.com>
2023-08-30 20:12:44 +0930

    add taliwind css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<button on:click={() => { count++; num++; }}>Count is: { count }</button>
-<p>random nuber: { num }</p>
+<script lang="ts">
+	let count: number = 0;
 
-<script>
-    let count = 0;
-    
-    let num = 0;
-    $: num += 1;
+	let num = 0;
+	$: num += 1;
+
+	let l = 'sd';
 </script>
+
+<h1 class="text-xl">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button
+	on:click={() => {
+		count++;
+		num++;
+	}}
+>
+	Count is: {count}
+</button>
+<p class="text-xl">random nuber: {num}</p>
