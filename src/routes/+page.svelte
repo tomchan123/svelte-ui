cab144000e59a9e0d46c8e7a7520e3fe64154c8d
Tom Chan <tomchan@qqueenyapp.com>
2023-08-30 18:08:37 +0930

    setup svelte kit scafflod

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={() => { count++; num++; }}>Count is: { count }</button>
+<p>random nuber: { num }</p>
+
+<script>
+    let count = 0;
+    
+    let num = 0;
+    $: num += 1;
+</script>
