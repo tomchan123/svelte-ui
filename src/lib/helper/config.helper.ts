a8c65577ba5fe23bdad169f2cf0dd6afe5aa2fdb
Tom Chan <tomchan@qqueenyapp.com>
2023-08-31 20:55:39 +0930

    move config file

--- src/lib/helper/config.helper.ts
+++ src/lib/helper/config.helper.ts
@@ -1,6 +1,6 @@
-import LandingNavBarItems from "$lib/config/landing-navbar-items.json";
+import config from "$lib/config.json" assert { type: "json " };
 import type { LandingNavBarItem } from "$lib/interface";
 
 export function getLandingNavBarItems(): LandingNavBarItem[] {
-    return LandingNavBarItems;
+    return config["landing_navbar_items"];
 }
