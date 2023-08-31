29ee56fb65109d2201af4b3c2465418d462a5aa8
Tom Chan <tomchan@qqueenyapp.com>
2023-08-31 21:11:06 +0930

    add landing page social config

--- src/lib/helper/config.helper.ts
+++ src/lib/helper/config.helper.ts
@@ -1,6 +1,10 @@
 import config from "$lib/config.json" assert { type: "json " };
-import type { LandingNavBarItem } from "$lib/interface";
+import type { LandingNavBarItem, LandingSocialItem } from "$lib/interface";
 
 export function getLandingNavBarItems(): LandingNavBarItem[] {
     return config["landing_navbar_items"];
+}
+
+export function getLandingSocialItems(): LandingSocialItem[] {
+    return config["landing_social_items"];
 }
--- src/lib/interface.ts
+++ src/lib/interface.ts
@@ -2,3 +2,8 @@ export interface LandingNavBarItem {
     text: string,
     url: string,
 }
+
+export interface LandingSocialItem {
+    faIcon: string,
+    url: string,
+}
